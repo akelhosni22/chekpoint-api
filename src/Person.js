9628dc25f0815d2d7d5b3245a3dd1dab87cb45b7
Akel <akel.hosni@gmail.com>
2021-06-24 11:10:25 +0100

    first commit

---
+++ src/Person.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Person = ({user}) => {
+    return (
+        <div>
+         
+           <h2> {user.name}</h2>
+      
+          
+        </div>
+    )
+}
+
+export default Person
--- src/UserList.js
+++ src/UserList.js
@@ -1,27 +1,26 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
+import Person from "./Person"
 
 const UserList = () => {
-    const [User, setUser] = useState([]);
+  const [User, setUser] = useState([]);
+  useEffect(() => {
     const GetUserList = () => {
-      
-        
-        axios.get("https://jsonplaceholder.typicode.com/users")
-  
-        .then((res) => {
-          setUser(res.data);
-        })
-        .catch((error) => console.log(`Error : ${error}`));
-    };
-  
-    useEffect(() => {
-      GetUserList();
-    });
-  
+      axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {setUser(res.data);
+      })
+      .catch((error) => console.log(error));
+  };
+    GetUserList();
+    console.log(User);
+    
+    
+  }, [])
     return (
       <div className="User">
-        {Data.map((el) => (
-          <User key={el.id} user={el.name} user ={el.mail} user={el.adress}/>
+        {User.map((el) => (
+          <Person user={el}/>
         ))}
       </div>
     );
